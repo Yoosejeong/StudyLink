21de284d61322be81e71ee951e1434b76effe7f1
YooSejeong <nived3@naver.com>
2025-07-23 21:28:52 +0900

    refactor: 스터디글 전체 조회 repository 메서드 변경을 위한 서비스 코드 변경

--- backend/src/main/java/com/studycrew/studyBoard/service/studyPost/StudyPostQueryService.java
+++ backend/src/main/java/com/studycrew/studyBoard/service/studyPost/StudyPostQueryService.java
@@ -1,11 +1,13 @@
 package com.studycrew.studyBoard.service.studyPost;
 
 import com.studycrew.studyBoard.dto.StudyPostDTO.StudyPostResponseDTO;
+import com.studycrew.studyBoard.dto.StudyPostDTO.StudyPostResponseDTO.GetStudyPostListResponse;
 import com.studycrew.studyBoard.entity.StudyPost;
+import com.studycrew.studyBoard.enums.StudyStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface StudyPostQueryService {
     StudyPost getStudyPost(Long studyPostId);
-    Page<StudyPostResponseDTO.GetStudyPostListResponse> getStudyPostList(Pageable pageable);
+    Page<GetStudyPostListResponse> getStudyPostList(StudyStatus status, Pageable pageable);
 }
