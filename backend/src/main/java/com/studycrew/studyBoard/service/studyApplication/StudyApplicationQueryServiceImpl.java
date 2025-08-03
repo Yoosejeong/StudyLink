33052d94b9008aa55f3ace491982dbb6d2fc4fdb
YooSejeong <nived3@naver.com>
2025-08-03 20:07:36 +0900

    feat: 지원 여부, 지원 상태 조회하는 서비스코드 추가

--- backend/src/main/java/com/studycrew/studyBoard/service/studyApplication/StudyApplicationQueryServiceImpl.java
+++ backend/src/main/java/com/studycrew/studyBoard/service/studyApplication/StudyApplicationQueryServiceImpl.java
@@ -1,15 +1,22 @@
 package com.studycrew.studyBoard.service.studyApplication;
 
 import com.studycrew.studyBoard.converter.StudyApplicationConverter;
+import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO;
 import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.MyStudyApplicationResponse;
 import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.StudyApplicationListResponse;
 import com.studycrew.studyBoard.entity.StudyApplication;
+import com.studycrew.studyBoard.enums.ApplicationStatus;
 import com.studycrew.studyBoard.repository.StudyApplicationRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.swing.text.html.Option;
+
+import static com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.*;
 
 @Transactional(readOnly = true)
 @Service
@@ -32,5 +39,15 @@ public class StudyApplicationQueryServiceImpl implements StudyApplicationQuerySe
         return userApplications.stream()
                 .map(StudyApplicationConverter::toUserApplications)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public HasAppliedResponse hasUserApplied(Long userId, Long studyPostId) {
+        Optional<ApplicationStatus> ApplicationStatus = studyApplicationRepository.findStatusByUserIdAndStudyPostId(
+                userId, studyPostId);
+        return HasAppliedResponse.builder()
+                .hasApplied(ApplicationStatus.isPresent())
+                .applicationStatus(ApplicationStatus.orElse(null))
+                .build();
     }
 }
