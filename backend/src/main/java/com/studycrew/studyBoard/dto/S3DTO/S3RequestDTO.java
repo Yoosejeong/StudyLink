ed800b64826811ac809c8a99093aad0531d9fcd5
YooSejeong <nived3@naver.com>
2025-08-25 16:13:09 +0900

    feat: s3 관련 요청, 응답 dto 추가

---
+++ backend/src/main/java/com/studycrew/studyBoard/dto/S3DTO/S3RequestDTO.java
@@ -0,0 +1,21 @@
+package com.studycrew.studyBoard.dto.S3DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class S3RequestDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PresignPutRequest {
+        @NotBlank
+        private String filename;
+        @NotBlank
+        private String contentType;
+    }
+}
