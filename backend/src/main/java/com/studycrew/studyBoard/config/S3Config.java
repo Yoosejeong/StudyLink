c407eff38b3edb65aed35495238a015aaf7f0d57
YooSejeong <nived3@naver.com>
2025-08-25 16:08:18 +0900

    feat: s3 설정 config 추가

---
+++ backend/src/main/java/com/studycrew/studyBoard/config/S3Config.java
@@ -0,0 +1,24 @@
+package com.studycrew.studyBoard.config;
+
+import com.studycrew.studyBoard.config.props.PresignProps;
+import com.studycrew.studyBoard.config.props.S3Props;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.providers.AwsRegionProvider;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@EnableConfigurationProperties({S3Props.class, PresignProps.class})
+public class S3Config {
+
+    @Bean
+    public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider,
+                                   AwsRegionProvider regionProvider) {
+        return S3Presigner.builder()
+                .credentialsProvider(credentialsProvider)
+                .region(regionProvider.getRegion())
+                .build();
+    }
+}
