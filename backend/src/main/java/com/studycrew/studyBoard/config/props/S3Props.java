c0ba9fce6a2d781c900d6a51e4a001ed10fe88e5
YooSejeong <nived3@naver.com>
2025-08-25 16:09:58 +0900

    feat: s3 설정 properties 추가

---
+++ backend/src/main/java/com/studycrew/studyBoard/config/props/S3Props.java
@@ -0,0 +1,7 @@
+package com.studycrew.studyBoard.config.props;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.s3")
+public record S3Props(String bucket) {
+}
