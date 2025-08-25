6ea66b47584cbf77062e37bb49a946e00944661d
YooSejeong <nived3@naver.com>
2025-08-25 16:12:02 +0900

    feat: header에 필요한 프로필 조회 응답 dto 추가

--- backend/src/main/java/com/studycrew/studyBoard/dto/UserDTO/UserResponseDTO.java
+++ backend/src/main/java/com/studycrew/studyBoard/dto/UserDTO/UserResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 public class UserResponseDTO {
 
     @Builder
@@ -16,6 +18,15 @@ public class UserResponseDTO {
         private String email;
         private String username;
         private String nickname;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class headerProfileDTO{
+        String profileUrl;
+        Instant expiresAt;
     }
 
 }
