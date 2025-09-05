bfd91ce3ad2deb61c4163c7471d7d0a8e32a7ab5
YooSejeong <nived3@naver.com>
2025-09-05 23:21:34 +0900

    refactor: 스터디글 조회에서 @EntityGraph로 user를 추가하여 같이 불러오기

--- backend/src/main/java/com/studycrew/studyBoard/repository/StudyPostRepository.java
+++ backend/src/main/java/com/studycrew/studyBoard/repository/StudyPostRepository.java
@@ -10,5 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyPostRepository extends StudyPostRepositoryCustom, JpaRepository<StudyPost, Long> {
+    @EntityGraph(attributePaths = "user")
     Optional<StudyPost> findByIdAndDeletedFalse(Long id);
 }
