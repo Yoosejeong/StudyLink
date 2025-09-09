8a991f2943e6ba181f695d418afe03383502b8b1
YooSejeong <nived3@naver.com>
2025-09-09 15:13:18 +0900

    feat: 스터디 카테고리 추가

---
+++ backend/src/main/java/com/studycrew/studyBoard/enums/Category.java
@@ -0,0 +1,12 @@
+package com.studycrew.studyBoard.enums;
+
+public enum Category {
+    BACKEND,
+    FRONTEND,
+    MOBILE,
+    DATA_AI,
+    CLOUD_DEVOPS,
+    ALGORITHM_CS,
+    GAME,
+    CERT;
+}
