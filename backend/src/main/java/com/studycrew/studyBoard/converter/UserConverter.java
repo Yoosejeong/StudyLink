02bd99e3c45fef55c489eacdead7fb995d0a00e6
YooSejeong <nived3@naver.com>
2025-10-01 19:45:20 +0900

    feat: 닉네임 응답 dto 생성하는 컨버터 코드 추가

--- backend/src/main/java/com/studycrew/studyBoard/converter/UserConverter.java
+++ backend/src/main/java/com/studycrew/studyBoard/converter/UserConverter.java
@@ -6,13 +6,21 @@ import com.studycrew.studyBoard.entity.User;
 
 public class UserConverter {
 
-    public static UserResponseDTO.getUserDTO toGetUserDTO(User user){
+    public static UserResponseDTO.getUserDTO toGetUserDTO(User user) {
         return UserResponseDTO.getUserDTO
                 .builder()
                 .userId(user.getId())
                 .email(user.getEmail())
                 .nickname(user.getNickname())
                 .username(user.getUsername())
+                .build();
+    }
+
+    public static UserResponseDTO.updateNicknameDTO toUpdateNicknameDTO(User user) {
+        return UserResponseDTO.updateNicknameDTO
+                .builder()
+                .userId(user.getId())
+                .nickname(user.getNickname())
                 .build();
     }
 
